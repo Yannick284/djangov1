4713fff8087db138c3992d8403a8caef1b611686
Yannick284 <yannickwahl@gmail.com>
2025-12-31 16:34:26 +0100

    div4

--- reels/migrations/0002_reel_thumbnail.py
+++ reels/migrations/0002_reel_thumbnail.py
@@ -1,25 +1,3 @@
-from django.db import migrations
-
-
-class Migration(migrations.Migration):
-
-    dependencies = [
-        ("reels", "0002_category_add_color"),  # <= NOM EXACT
-    ]
-
-    operations = [
-        migrations.SeparateDatabaseAndState(
-            database_operations=[
-                migrations.RunSQL(
-                    sql='ALTER TABLE "reels_category" DROP COLUMN IF EXISTS "color";',
-                    reverse_sql=migrations.RunSQL.noop,
-                )
-            ],
-            state_operations=[
-                migrations.RemoveField(
-                    model_name="category",
-                    name="color",
-                )
-            ],
-        )
-    ]
+dependencies = [
+    ("reels", "0001_initial"),
+]
