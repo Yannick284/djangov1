6d45ee06d011a7b03f1c6296bf0a1d2efdc42a76
Yannick284 <yannickwahl@gmail.com>
2025-12-31 17:26:30 +0100

    div11

--- my_site/settings.py
+++ my_site/settings.py
@@ -173,7 +173,7 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 
 DATABASES = {
 
-         "default": dj_database_url.config(conn_max_age=600)
+          "default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))
     
 }
 
